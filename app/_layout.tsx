3c6535d4fbe4a885c55e9fe0e8b9dd80edae8a87
Loukas <loukas.bass@gmail.com>
2024-04-25 06:45:46 +0300

    first commit

---
+++ app/_layout.tsx
@@ -0,0 +1,19 @@
+import { Stack } from "expo-router";
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from "expo-router";
+
+export const unstable_settings = {
+  // Ensure that reloading on `/modal` keeps a back button present.
+  initialRouteName: "(tabs)",
+};
+
+export default function RootLayoutNav() {
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{}} />
+    </Stack>
+  );
+}
